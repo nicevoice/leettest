ebd94ff5a600af389d21f8b37fb00a7848d72b95
wangpeng <buaawp@gmail.com>
2015-04-27 10:09:42 +0800

    update image configuration for tool module

--- leettest/urls.py
+++ leettest/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
 
+from leettest import settings
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -21,6 +23,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
         
     url(r'^comments/', include('django.contrib.comments.urls')),
+    
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
     
     #url(r'^static/(?P<path>.*)$', 'django.views.static.serve',    {'document_root': 'media'}),
     
--- tool/models.py
+++ tool/models.py
@@ -1,11 +1,12 @@
+#coding:utf-8
 from django.db import models
 
 class Tool(models.Model):
     name=models.CharField(max_length=20)
-    summary=models.CharField(max_length=400)
+    summary=models.CharField(max_length=200)
     description=models.TextField()
-    logo=models.ImageField()
-    picture=models.ImageField()   
+    logo=models.ImageField(upload_to='photos')
+    picture=models.ImageField(upload_to='photos')   
     
     def __unicode__(self):
         return self.name

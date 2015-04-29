49c1b874763ed12f57749cf5a32f85cab6330a9e
wangpeng <buaawp@gmail.com>
2015-04-29 20:12:24 +0800

    add RichTextEditor for tool , add back button in tool_show page

--- tool/admin.py
+++ tool/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from tool.models import Tool
 
+class ToolAdmin(admin.ModelAdmin):
+    class Media:
+        js = (
+            '/static/js/tiny_mce/tiny_mce_src.js', 
+            '/static/js/tiny_mce/tiny_mce_config.js'
+            )
 
-admin.site.register(Tool)
+admin.site.register(Tool,ToolAdmin)

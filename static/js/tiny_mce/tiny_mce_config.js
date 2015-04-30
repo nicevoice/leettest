507309c1d9b6cbef2126896f3bfb06f424184543
wangpeng <buaawp@gmail.com>
2015-04-30 13:39:59 +0800

    add syntaxhighlighter for tinymce

--- static/js/tiny_mce/tiny_mce_config.js
+++ static/js/tiny_mce/tiny_mce_config.js
@@ -2,12 +2,13 @@
         // General options  
         mode : "textareas",  
         theme : "advanced",  
-        plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave",  
+        plugins : "syntaxhl,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave",  
        
         // Theme options  
         theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect,fullscreen,code",  
         theme_advanced_buttons2 : "cut,copy,paste,pastetext,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,|,insertdate,inserttime,preview,|,forecolor,backcolor",  
         theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl",  
+        theme_advanced_buttons4 : "syntaxhl",  
        
         theme_advanced_toolbar_location : "top",  
         theme_advanced_toolbar_align : "left",  

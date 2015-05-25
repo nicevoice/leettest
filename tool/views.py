50e856e4f1df49447772b626c0791056702aadfc
wangpeng <buaawp@gmail.com>
2015-05-25 14:58:10 +0800

    add filter for Tag in tool module

--- tool/views.py
+++ tool/views.py
@@ -5,10 +5,29 @@ from django.template.context import RequestContext
 from django.http import Http404
 
 from tool.models import Tool
+from public.models import Tag
 
 
 def list(request):
-    all_tools = Tool.objects.all()
+    all_tools = Tool.objects.all().order_by("-id")
+
+    total_num=len(all_tools)
+    
+    tags=Tag.objects.all()
+
+    for tag in tags:
+        tag.tool_num=len(tag.tool_set.all())
+        tag.save()
+
+    tagid = request.GET.get('tagid')  
+    if tagid:
+        # 从request中获取的tagid为字符型变量，需要转为int型，
+        # 否则在template中无法与tag.id做equal判断
+        tagid=int(tagid) 
+        
+        all_tools = Tag.objects.get(id=tagid).tool_set.all().order_by("-id")
+        
+
     
     p = Paginator(all_tools , 7)
     
@@ -33,13 +52,23 @@ def list(request):
         page_range = p.page_range[page - after_range_num:page + before_range_num]
     else:
         page_range = p.page_range[0:page + before_range_num]
-        
+
+
     return render_to_response('tool_list.html', locals())
 
 def detail(request,tool_id):
+
+    total_num=len(Tool.objects.all())
+
     try:
-        tool = Tool.objects.get(id=tool_id)        
+        tool = Tool.objects.get(id=tool_id)
+
+        tags=tool.tags.all()
+        for tag in tags:
+            tag.tool_num=len(tag.tool_set.all())
+            tag.save()
+
     except Tool.DoesNotExist:
         raise Http404("工具不存在！")
-    return render_to_response('tool_detail.html', {"tool": tool},context_instance=RequestContext(request))
+    return render_to_response('tool_detail.html', locals(),context_instance=RequestContext(request))
 

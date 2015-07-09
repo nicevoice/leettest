7d3b113343bfd699fd958ba2f358e651a9bea26d
Peng Wang <buaawp@gmail.com>
2015-07-09 17:11:37 +0800

    add scroll_to_top button in index,remove all banners

--- index/views.py
+++ index/views.py
@@ -10,30 +10,7 @@ def index(req):
 	
 	if not req.user.is_authenticated():
 		all_articles=all_articles.filter(ispublic=1)
-
-	p = Paginator(all_articles , 7)
-    
-	try:
-	    page = int(req.GET.get('page',1))
-	    if page < 1:
-	        page = 1
-	except ValueError:
-	    page = 1  
-	    
-	try:
-	    articles = p.page(page)
-	except PageNotAnInteger: 
-	    articles = p.page(1)
-	except EmptyPage: 
-	    articles = p.page(p.num_pages)
-	    
-	after_range_num = 3
-	before_range_num = 2
-	 
-	if page >= after_range_num:
-	    page_range = p.page_range[page - after_range_num:page + before_range_num]
-	else:
-	    page_range = p.page_range[0:page + before_range_num]   
-        
+	p = Paginator(all_articles , 10)
+	articles = p.page(1)        
 
 	return render_to_response("index.html",locals())

56d812b4ff0230a74df14c91a28400cb950ef203
Peng Wang <buaawp@gmail.com>
2015-07-10 14:45:27 +0800

    add styles for rank_num in index_page

--- index/views.py
+++ index/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render_to_response
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from index.models import Block
-from blog.models import Article
+from blog.models import Article,Category
 
 def index(req):
 	blocks=Block.objects.all().order_by("-id")
 
 	all_articles = Article.objects.all().order_by("-publish_time")
+
+	categorys=Category.objects.all()
+    
+	for category in categorys:
+		category.article_num = 0
+		for article in all_articles:
+			if article.category == category:
+				category.article_num += 1
+		category.save()
+
 	
 	if not req.user.is_authenticated():
 		all_articles=all_articles.filter(ispublic=1)
